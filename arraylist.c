1421980e58738a714261880b0a93040062edd43b
Replit user <>
2023-03-31 15:08:23 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -63,9 +63,9 @@ void push(ArrayList *l, void *data, int i) {
 }
 
 void *pop(ArrayList *l, int i) {
-  if (i > l->size)
+  if (l->capacity==0 || i > l->size)
     return NULL;
-
+	
   if (i<0)
   	i= l->size-i;
 	
